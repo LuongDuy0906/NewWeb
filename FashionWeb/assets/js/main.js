d91d2ee66397249bce63accdff483584e10fd88d
LuongDuy0906 <luognducduy0906@gmail.com>
2025-05-31 15:52:34 +0700

    Update web

--- FashionWeb/assets/js/main.js
+++ FashionWeb/assets/js/main.js
@@ -19,18 +19,26 @@ var swiperCategories = new Swiper(".categories__container", {
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1400: {
-          slidesPerView: 6,
-          spaceBetween: 24,
-        },
+      350: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 224,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 24,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 24,
+      },
     },
 });
 
@@ -42,13 +50,13 @@ var swiperProducts = new Swiper(".new__container", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
       768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 24,
       },
       1400: {
         slidesPerView: 4,
