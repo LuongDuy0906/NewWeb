5992f87c098fb66ccafdcdc476300ba52abf3229
LuongDuy0906 <luognducduy0906@gmail.com>
2025-06-17 09:00:53 +0700

    Update final web

--- FashionWeb/assets/js/products.js
+++ FashionWeb/assets/js/products.js
@@ -1,98 +1,98 @@
 const products = [
-    {
-      id: 'product-1',
-      name: 'Modern T-Shirt',
-      price: 59.99,
-      oldPrice: 75.00,
-      image: 'assets/img/product-1-1.jpg',
-      imageHover: 'assets/img/product-1-2.jpg'
-    },
-    {
-      id: 'product-2',
-      name: 'Elegant Blazer',
-      price: 89.99,
-      oldPrice: 110.00,
-      image: 'assets/img/product-2-1.jpg',
-      imageHover: 'assets/img/product-2-2.jpg'
-    },
-    {
-      id: 'product-3',
-      name: 'Casual Jeans',
-      price: 69.50,
-      oldPrice: 85.00,
-      image: 'assets/img/product-3-1.jpg',
-      imageHover: 'assets/img/product-3-2.jpg'
-    },
-    {
-      id: 'product-4',
-      name: 'Hooded Sweatshirt',
-      price: 49.00,
-      oldPrice: 65.00,
-      image: 'assets/img/product-4-1.jpg',
-      imageHover: 'assets/img/product-4-2.jpg'
-    },
-    {
-      id: 'product-5',
-      name: 'Summer Shorts',
-      price: 39.99,
-      oldPrice: 50.00,
-      image: 'assets/img/product-5-1.jpg',
-      imageHover: 'assets/img/product-5-2.jpg'
-    },
-    {
-      id: 'product-6',
-      name: 'Slim Fit Polo',
-      price: 45.00,
-      oldPrice: 55.00,
-      image: 'assets/img/product-6-1.jpg',
-      imageHover: 'assets/img/product-6-2.jpg'
-    },
-    {
-      id: 'product-7',
-      name: 'Streetwear Jacket',
-      price: 120.00,
-      oldPrice: 145.00,
-      image: 'assets/img/product-7-1.jpg',
-      imageHover: 'assets/img/product-7-2.jpg'
-    },
-    {
-      id: 'product-8',
-      name: 'Linen Shirt',
-      price: 62.00,
-      oldPrice: 78.00,
-      image: 'assets/img/product-8-1.jpg',
-      imageHover: 'assets/img/product-8-2.jpg'
-    },
-    {
-      id: 'product-9',
-      name: 'Denim Jacket',
-      price: 95.00,
-      oldPrice: 120.00,
-      image: 'assets/img/product-9-1.jpg',
-      imageHover: 'assets/img/product-9-2.jpg'
-    },
-    {
-      id: 'product-10',
-      name: 'Cotton Hoodie',
-      price: 58.50,
-      oldPrice: 72.00,
-      image: 'assets/img/product-10-1.jpg',
-      imageHover: 'assets/img/product-10-2.jpg'
-    },
-    {
-      id: 'product-11',
-      name: 'Stylish Coat',
-      price: 140.00,
-      oldPrice: 160.00,
-      image: 'assets/img/product-11-1.jpg',
-      imageHover: 'assets/img/product-11-2.jpg'
-    },
-    {
-      id: 'product-12',
-      name: 'Sport Sneakers',
-      price: 105.00,
-      oldPrice: 130.00,
-      image: 'assets/img/product-12-1.jpg',
-      imageHover: 'assets/img/product-12-2.jpg'
-    }
-  ];
+  {
+    id: 'product-1',
+    name: 'Áo thun nhiều màu sắc',
+    price: 350.000,
+    oldPrice: 560.000,
+    image: 'assets/img/ChatGPT Image Jun 15, 2025, 01_21_46 PM.png',
+    imageHover: 'assets/img/ChatGPT Image Jun 15, 2025, 01_21_56 PM.png'
+  },
+  {
+    id: 'product-2',
+    name: 'Áo thun nhiều màu sắc',
+    price: 350.000,
+    oldPrice: 560.000,
+    image: 'assets/img/ChatGPT Image Jun 15, 2025, 01_23_35 PM.png',
+    imageHover: 'assets/img/ChatGPT Image Jun 15, 2025, 01_23_37 PM.png'
+  },
+  {
+    id: 'product-3',
+    name: 'Giày thể thao nhiều màu sắc',
+    price: 585.000,
+    oldPrice: 780.000,
+    image: 'assets/img/ChatGPT Image Jun 15, 2025, 01_33_40 PM.png',
+    imageHover: 'assets/img/snapedit_1749969204279.jpeg'
+  },
+  {
+    id: 'product-4',
+    name: 'Quần vải hoa văn nam',
+    price: 475.000,
+    oldPrice: 675.000,
+    image: 'assets/img/snapedit_1749969411763.jpeg',
+    imageHover: 'assets/img/snapedit_1749969483184.jpeg'
+  },
+  {
+    id: 'product-5',
+    name: 'Mũ len nhiều họa tiết',
+    price: 550.000,
+    oldPrice: 1500.000,
+    image: 'assets/img/product-5-1.png',
+    imageHover: 'assets/img/product-5-2.png'
+  },
+  {
+    id: 'product-6',
+    name: 'Áo thun nhiều màu sắc',
+    price: 350.000,
+    oldPrice: 560.000,
+    image: 'assets/img/product-6-1.png',
+    imageHover: 'assets/img/product-6-2.png'
+  },
+  {
+    id: 'product-7',
+    name: 'Áo lụa nhiều màu sắc',
+    price: 875.000,
+    oldPrice: 1300.000,
+    image: 'assets/img/product-7-1.jpg',
+    imageHover: 'assets/img/product-7-2.jpg'
+  },
+  {
+    id: 'product-8',
+    name: 'Áo sơ mi nhiều màu sắc',
+    price: 1200.000 ,
+    oldPrice: 2300.000,
+    image: 'assets/img/product-8-1.jpg',
+    imageHover: 'assets/img/product-8-2.jpg'
+  },
+  {
+    id: 'product-9',
+    name: 'Áo thun nhiều màu sắc',
+    price: 350.000,
+    oldPrice: 560.000,
+    image: 'assets/img/product-9-1.png',
+    imageHover: 'assets/img/product-9-2.png'
+  },
+  {
+    id: 'product-10',
+    name: 'Dép nhiều họa tiết',
+    price: 340.000,
+    oldPrice: 500.000,
+    image: 'assets/img/product-10-1.png',
+    imageHover: 'assets/img/product-10-2.png'
+  },
+  {
+    id: 'product-11',
+    name: 'Quần vải hoa văn nữ',
+    price: 475.000,
+    oldPrice: 675.000,
+    image: 'assets/img/product-11-1.jpg',
+    imageHover: 'assets/img/product-11-2.jpg'
+  },
+  {
+    id: 'product-12',
+    name: 'Áo thun nhiều màu sắc',
+    price: 350.000,
+    oldPrice: 560.000,
+    image: 'assets/img/product-12-1.jpg',
+    imageHover: 'assets/img/product-12-2.jpg'
+  }
+];
