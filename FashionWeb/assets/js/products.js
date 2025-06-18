ed1b9c61cb722ed6b8832f6ab63f40a771d5fef1
LuongDuy0906 <luongducduy0906@gmail.com>
2025-06-18 09:41:45 +0700

    update final

--- FashionWeb/assets/js/products.js
+++ FashionWeb/assets/js/products.js
@@ -2,96 +2,96 @@ const products = [
   {
     id: 'product-1',
     name: 'Áo thun nhiều màu sắc',
-    price: 350.000,
-    oldPrice: 560.000,
+    price: 350000,
+    oldPrice: 560000,
     image: 'assets/img/ChatGPT Image Jun 15, 2025, 01_21_46 PM.png',
     imageHover: 'assets/img/ChatGPT Image Jun 15, 2025, 01_21_56 PM.png'
   },
   {
     id: 'product-2',
     name: 'Áo thun nhiều màu sắc',
-    price: 350.000,
-    oldPrice: 560.000,
+    price: 350000,
+    oldPrice: 560000,
     image: 'assets/img/ChatGPT Image Jun 15, 2025, 01_23_35 PM.png',
     imageHover: 'assets/img/ChatGPT Image Jun 15, 2025, 01_23_37 PM.png'
   },
   {
     id: 'product-3',
     name: 'Giày thể thao nhiều màu sắc',
-    price: 585.000,
-    oldPrice: 780.000,
+    price: 585000,
+    oldPrice: 780000,
     image: 'assets/img/ChatGPT Image Jun 15, 2025, 01_33_40 PM.png',
     imageHover: 'assets/img/snapedit_1749969204279.jpeg'
   },
   {
     id: 'product-4',
     name: 'Quần vải hoa văn nam',
-    price: 475.000,
-    oldPrice: 675.000,
+    price: 475000,
+    oldPrice: 675000,
     image: 'assets/img/snapedit_1749969411763.jpeg',
     imageHover: 'assets/img/snapedit_1749969483184.jpeg'
   },
   {
     id: 'product-5',
     name: 'Mũ len nhiều họa tiết',
-    price: 550.000,
-    oldPrice: 1500.000,
+    price: 550000,
+    oldPrice: 1500000,
     image: 'assets/img/product-5-1.png',
     imageHover: 'assets/img/product-5-2.png'
   },
   {
     id: 'product-6',
     name: 'Áo thun nhiều màu sắc',
-    price: 350.000,
-    oldPrice: 560.000,
+    price: 350000,
+    oldPrice: 560000,
     image: 'assets/img/product-6-1.png',
     imageHover: 'assets/img/product-6-2.png'
   },
   {
     id: 'product-7',
     name: 'Áo lụa nhiều màu sắc',
-    price: 875.000,
-    oldPrice: 1300.000,
+    price: 875000,
+    oldPrice: 1300000,
     image: 'assets/img/product-7-1.jpg',
     imageHover: 'assets/img/product-7-2.jpg'
   },
   {
     id: 'product-8',
     name: 'Áo sơ mi nhiều màu sắc',
-    price: 1200.000 ,
-    oldPrice: 2300.000,
+    price: 1200000 ,
+    oldPrice: 2300000,
     image: 'assets/img/product-8-1.jpg',
     imageHover: 'assets/img/product-8-2.jpg'
   },
   {
     id: 'product-9',
     name: 'Áo thun nhiều màu sắc',
-    price: 350.000,
-    oldPrice: 560.000,
+    price: 350000,
+    oldPrice: 560000,
     image: 'assets/img/product-9-1.png',
     imageHover: 'assets/img/product-9-2.png'
   },
   {
     id: 'product-10',
     name: 'Dép nhiều họa tiết',
-    price: 340.000,
-    oldPrice: 500.000,
+    price: 340000,
+    oldPrice: 500000,
     image: 'assets/img/product-10-1.png',
     imageHover: 'assets/img/product-10-2.png'
   },
   {
     id: 'product-11',
     name: 'Quần vải hoa văn nữ',
-    price: 475.000,
-    oldPrice: 675.000,
+    price: 475000,
+    oldPrice: 675000,
     image: 'assets/img/product-11-1.jpg',
     imageHover: 'assets/img/product-11-2.jpg'
   },
   {
     id: 'product-12',
     name: 'Áo thun nhiều màu sắc',
-    price: 350.000,
-    oldPrice: 560.000,
+    price: 350000,
+    oldPrice: 560000,
     image: 'assets/img/product-12-1.jpg',
     imageHover: 'assets/img/product-12-2.jpg'
   }
